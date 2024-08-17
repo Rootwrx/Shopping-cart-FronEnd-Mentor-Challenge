7301671e8ef86495143a206e621e9e056cfebd03
Rootwrx <smailettanjaoui@gmail.com>
2024-08-17 22:28:25 +0100

    Update lazyloader.js
    fixing,optimizing,

--- src/js/modules/lazyloader.js
+++ src/js/modules/lazyloader.js
@@ -1,88 +1,48 @@
-// class LazyLoader {
-//   constructor({ selector = ".lazy-load", options, loadCallback = null } = {}) {
-//     this.selector = selector;
-//     this.loadCallback = loadCallback;
-//     this.images = document.querySelectorAll(this.selector);
-//     this.observer = null;
-//     this.options = options || {
-//       root: null,
-//       rootMargin: "0px",
-//       threshold: 0.01,
-//     };
-
-//     this.init();
-//   }
-
-//   init() {
-//     if ("IntersectionObserver" in window) {
-//       this.observer = new IntersectionObserver(
-//         this.onIntersection.bind(this),
-//         this.options
-//       );
-
-//       this.images.forEach((image) => {
-//         if (!image.classList.contains("loaded")) this.observer.observe(image);
-//       });
-//     } else this.loadImagesFallback();
-//   }
-
-//   onIntersection(entries, observer) {
-//     entries.forEach((entry) => {
-//       if (entry.isIntersecting) {
-//         this.loadImage(entry.target);
-//         observer.unobserve(entry.target);
-//       }
-//     });
-//   }
-
-//   loadImage(image) {
-//     const src = image.dataset.src;
-//     const srcset = image.dataset.srcset;
-
-//     if (src) image.src = src;
-//     if (srcset) image.srcset = srcset;
-
-//     image.onload = () => {
-//       console.log("loaded");
-//       image.classList.add("loaded");
-//       image.classList.remove(this.selector.replace(".", ""));
-//       if (typeof this.loadCallback === "function") this.loadCallback(image);
-//     };
-//   }
-
-//   loadImagesFallback() {
-//     this.images.forEach((image) => this.loadImage(image));
-//   }
-
-//   refresh() {
-//     this.images = document.querySelectorAll(this.selector);
-//     this.images.forEach((image) => {
-//       if (!image.classList.contains("loaded")) {
-//         this.observer.observe(image);
-//       }
-//     });
-//   }
-// }
-
-// export default LazyLoader;
-
 class LazyLoader {
   constructor(options = {}) {
     this.options = {
-      rootMargin: "100px",
-      threshold: 0.1,
       loadingClass: "loading",
       loadedClass: "loaded",
-      errorClass: "error",
+      selector: ".lazy-load",
+      errorClass: "failed",
+      retryAfter: 2000,
+      maxRetries: 4,
+      loadCallback: null,
+      rootMargin: "0px 0px 100px 0px",
+      threshold: 0.1,
       ...options,
     };
+    // prettier-ignore
+    this.selector = `${this.options.selector.trim()}:not([data-status="${this.options.loadedClass}"],[data-status="${this.options.loadingClass}"])`;
 
-    this.observer = null;
     this.elements = new Map();
+    this.observer = null;
+    this.lazyImgs = [];
     this.init();
   }
 
+  handleIntersection(entries) {
+    if (!this.observer) return;
+    for (const entry of entries) {
+      if (entry.isIntersecting) {
+        this.loadElement(entry.target);
+      }
+    }
+
+    if (this.elements.size === 0) {
+      this.observer.disconnect();
+      this.observer = null;
+    }
+  }
   init() {
+    this.lazyImgs = Array.from(
+      document.querySelectorAll(this.options.selector)
+    );
+    if (!"IntersectionObserver" in window) {
+      console.warn("IntersectionObserver is not supported by this browser.");
+      this.loadImagesFallback();
+      return;
+    }
     this.observer = new IntersectionObserver(
       this.handleIntersection.bind(this),
       {
@@ -90,162 +50,130 @@ class LazyLoader {
         threshold: this.options.threshold,
       }
     );
+    this.observe();
   }
 
-  observe(elements) {
-    if (!Array.isArray(elements)) {
-      elements = Array.from(elements);
-    }
+  observe() {
+    this.lazyImgs.forEach((element) => {
+      if (this.elements.has(element)) return;
 
-    elements.forEach((element) => {
-      if (!this.elements.has(element)) {
-        this.elements.set(element, {
-          src: element.dataset.src,
-          srcset: element.dataset.srcset,
-          loaded: false,
-        });
-        this.observer.observe(element);
+      this.elements.set(element, {
+        src: element.dataset?.src,
+        srcset: element.dataset?.srcset,
+        status: null,
+      });
+
+      this.observer.observe(element);
+    });
+  }
+
+  loadImagesFallback() {
+    this.lazyImgs.forEach((image) => image && this.loadElement(image));
+  }
+
+  async loadElement(element) {
+    const data = this.elements.get(element);
+    if (!data) return;
+    const { loadingClass, loadedClass, loadCallback } = this.options;
+    if (data.status == loadingClass) {
+      console.log(element);
+    }
+    if (
+      data.status == loadingClass ||
+      data.status == loadedClass ||
+      element.dataset.status == loadedClass
+    )
+      return;
+    data.status = element.dataset.status = loadingClass;
+    element.classList.add(loadingClass);
+
+    try {
+      if (element.tagName.toLowerCase() === "img")
+        await this.loadImg(element, data);
+      else await this.loadBgImg(element, data);
+
+      element.classList.replace(loadingClass, loadedClass);
+      element.removeAttribute("data-src");
+      element.removeAttribute("data-srcset");
+
+      data.status = element.dataset.status = loadedClass;
+
+      if (typeof loadCallback === "function") {
+      }
+      loadCallback?.();
+      this.unobserve(element);
+    } catch (error) {
+      this.handleError(element, data);
+      throw error;
+    }
+  }
+
+  handleError(element, data) {
+    if (!data.retries) data.retries = 1;
+    // removing 'loading status' to allow retrying
+    delete data.status;
+    if (data.retries < this.options.maxRetries) {
+      console.log("Retrying loading ", element);
+      data.retries += 1;
+      setTimeout(() => this.loadElement(element), this.options.retryAfter);
+    } else this.markAsError(element);
+  }
+
+  markAsError(element) {
+    element.dataset.status = "failed";
+    element.classList.replace(
+      this.options.loadingClass,
+      this.options.errorClass
+    );
+    this.unobserve(element);
+    element.removeAttribute("data-src");
+    element.removeAttribute("data-srcset");
+  }
+
+  unobserve(element) {
+    this.observer.unobserve(element);
+    this.elements.delete(element);
+  }
+
+  loadImg(img, data) {
+    return new Promise((resolve, reject) => {
+      if (data.src) img.src = data.src;
+      if (data.srcset) img.srcset = data.srcset;
+
+      // if image is already downloaded or cached before it intersects the viewport
+      if (img.complete) resolve();
+      else {
+        img.onload = resolve;
+        img.onerror = () =>
+          reject(new Error(`Failed to load image: ${data.src || img.srcset}`));
       }
     });
   }
 
-  unobserve(element) {
-    if (this.elements.has(element)) {
-      this.observer.unobserve(element);
-      this.elements.delete(element);
-    }
-  }
-
-  handleIntersection(entries) {
-    entries.forEach((entry) => {
-      if (entry.isIntersecting) this.loadElement(entry.target);
-    });
-  }
-
-  async loadElement(element) {
-    const elementData = this.elements.get(element);
-    if (!elementData || elementData.loaded) return;
-
-    element.classList.add(this.options.loadingClass);
-
-    try {
-      if (element.tagName.toLowerCase() === "img")
-        await this.loadImage(element, elementData);
-      else await this.loadBackground(element, elementData);
-
-      element.classList.remove(this.options.loadingClass);
-      element.classList.add(this.options.loadedClass);
-      elementData.loaded = true;
-      this.unobserve(element);
-    } catch (error) {
-      console.error("Error loading element:", error);
-      element.classList.remove(this.options.loadingClass);
-      element.classList.add(this.options.errorClass);
-    }
-  }
-
-  loadImage(img, data) {
-    return new Promise((resolve, reject) => {
-      img.onload = resolve;
-      img.onerror = reject;
-      if (data.srcset) img.srcset = data.srcset;
-      img.src = data.src;
-    });
-  }
-
-  loadBackground(element, data) {
+  loadBgImg(element, data) {
     return new Promise((resolve, reject) => {
       const img = new Image();
-      img.onload = () => {
+
+      if (img.complete) {
         element.style.backgroundImage = `url(${data.src})`;
         resolve();
-      };
-      img.onerror = reject;
-      img.src = data.src;
-    });
-  }
-
-  refresh() {
-    this.elements.forEach((data, element) => {
-      if (!data.loaded) {
-        this.observer.unobserve(element);
-        this.observer.observe(element);
+      } else {
+        img.onload = () => {
+          element.style.backgroundImage = `url(${data.src})`;
+          resolve();
+        };
+        img.onerror = () =>
+          reject(new Error(`Failed to load background image: ${data.src}`));
       }
     });
   }
 
   destroy() {
-    this.observer.disconnect();
+    if (this.observer) {
+      this.observer.disconnect();
+      this.observer = null;
+    }
     this.elements.clear();
   }
 }
-
-export { LazyLoader };
-/*
- in case of  letting image load without using actualy lazyload
- so show loader before the image is loaded 
-@mixin lazy-loader(
-  $loader-size: 50px,
-  $loader-border: 5px,
-  $loader-color: var(--clr-red),
-  $loader-bg: var(--clr-rose-50)
-) {
-  &.loader {
-    position: relative;
-    overflow: hidden;
-
-    &::before {
-      content: "";
-      position: absolute;
-      top: 50%;
-      left: 50%;
-      transform: translate(-50%, -50%);
-      height: $loader-size;
-      width: $loader-size;
-      margin: auto;
-      border: $loader-border solid transparent;
-      border-left-color: $loader-color;
-      z-index: 10;
-      border-radius: 50%;
-      animation: loaderspin 1s linear infinite;
-    }
-
-    &::after {
-      content: "";
-      position: absolute;
-      height: 100%;
-      width: 100%;
-      background-color: $loader-bg;
-      z-index: 5;
-      top: 0;
-      left: 0;
-    }
-    &:has(.loaded) {
-      &::before,
-      &::after {
-        opacity: 0;
-        visibility: hidden;
-        transition: opacity 0.5s ease-in, visibility 0.5s ease-in;
-      }
-    }
-  }
-
-  img.lazy-load {
-    opacity: 0;
-    transition: opacity 0.5s ease-in;
-
-    &.loaded {
-      opacity: 1;
-    }
-  }
-}
-
-@keyframes loaderspin {
-  from {
-    transform: rotate(0deg);
-  }
-  to {
-    transform: rotate(360deg);
-  }
-}
-*/
+export default LazyLoader;
